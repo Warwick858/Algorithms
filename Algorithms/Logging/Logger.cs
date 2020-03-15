47ae4a9ccc2aa207d585bbd4774c0d0b933d3041
Warwick858 <lo40ops@gmail.com>
2020-03-14 20:48:05 -0600

    Add project files.

---
+++ Algorithms/Logging/Logger.cs
@@ -0,0 +1,53 @@
+﻿// ******************************************************************************************************************
+//  This file is part of Algorithms.
+//
+//  Algorithms - collection of various algorithms.
+//  Copyright(C)  2020  James LoForti
+//  Contact Info: jamesloforti@gmail.com
+//
+//  Algorithms is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program. If not, see<https://www.gnu.org/licenses/>.
+//									     ____.           .____             _____  _______   
+//									    |    |           |    |    ____   /  |  | \   _  \  
+//									    |    |   ______  |    |   /  _ \ /   |  |_/  /_\  \ 
+//									/\__|    |  /_____/  |    |__(  <_> )    ^   /\  \_/   \
+//									\________|           |_______ \____/\____   |  \_____  /
+//									                             \/          |__|        \/ 
+//
+// ******************************************************************************************************************
+//
+using System.Diagnostics;
+
+namespace Algorithms.Logging
+{
+	public static class Logger
+	{
+		private static string _logPath = @"D:\Documents\Computer Science\Algorithms\C#\Algorithms\Algorithms\Logs\log.txt";
+
+		public static void Write(string msg)
+		{
+			TextWriterTraceListener output = new TextWriterTraceListener(_logPath, "log");
+			output.Write(msg);
+			output.Flush();
+			output.Dispose();
+		}
+
+		public static void WriteLine(string msg)
+		{
+			TextWriterTraceListener output = new TextWriterTraceListener(_logPath, "log");
+			output.WriteLine(msg);
+			output.Flush();
+			output.Dispose();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+[![Jimmy LoForti Portfolio Website](http://jimmyloforti.com/_common/images/jrl_logo2.png)](http://jimmyloforti.com)
+
+## Application Name: Algorithms ##
+
+* Framework: .NET Core 3.1
+
+* Solution Type(s): Class Library, NUnit
+
+* Description: collection of various algorithms.
+
+* Nuget Packages:
+	* Microsoft.NET.Test.Sdk - 16.2.0
+	* NUnit - 3.12.0
+	* NUnit3TestAdapter - 3.15.1
+	
+-----------------------------------------------------------------------------------------------
+
+* Algorithms
+	* BubbleSort
+	* QuickSort
+	* ShellSort
+	* BinarySearch
+	
+-----------------------------------------------------------------------------------------------
